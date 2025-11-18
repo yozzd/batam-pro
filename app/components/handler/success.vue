08441cd62c4dcedacdc167ed0d539eeed16503bd
Yossie Dharma Putra <yozzdharma@gmail.com>
2025-11-18 16:27:38 +0700

    feat: buat komponen SuccessHandler untuk success messages

---
+++ app/components/handler/success.vue
@@ -0,0 +1,22 @@
+<!-- app/components/handler/success.vue -->
+
+<script setup>
+import { CheckCircle } from 'lucide-vue-next';
+
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <Alert variant="success">
+    <CheckCircle />
+    <AlertTitle>Sukses!</AlertTitle>
+    <AlertDescription>
+      {{ message }}
+    </AlertDescription>
+  </Alert>
+</template>
